9003dfd97ad70310bd4e314b128187d5d4351425
harkce <habibgarut@gmail.com>
2017-11-16 03:33:27 +0700

    Disable timestamps

--- app/User.php
+++ app/User.php
@@ -6,7 +6,7 @@ use Illuminate\Foundation\Auth\User as Authenticatable;
 
 class User extends Authenticatable
 {
-
+    public $timestamps = false;
     /**
      * The attributes that are mass assignable.
      *
