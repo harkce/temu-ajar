df695378d55ad3d0ea6e91c99c194cdbd165b384
harkce <habibgarut@gmail.com>
2017-11-16 02:57:08 +0700

    Add standard json response

--- app/Http/Controllers/MainController.php
+++ app/Http/Controllers/MainController.php
@@ -6,5 +6,9 @@ use Illuminate\Http\Request;
 
 class MainController extends Controller
 {
-    //
+    protected function jsonResponse($status, $message, $content, $code = 200) {
+    	return response()
+            ->json(['code' => $status,'message' => $message, 'content' => $content], $code)
+            ->header('Content-Type', 'application/json');
+    }
 }
