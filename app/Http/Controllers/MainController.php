848e016097bbde07b48ad428c23c629324ada56d
harkce <habibgarut@gmail.com>
2017-11-16 03:32:48 +0700

    Set default value for content

--- app/Http/Controllers/MainController.php
+++ app/Http/Controllers/MainController.php
@@ -6,7 +6,7 @@ use Illuminate\Http\Request;
 
 class MainController extends Controller
 {
-    protected function jsonResponse($status, $message, $content, $code = 200) {
+    protected function jsonResponse($status, $message, $content = null, $code = 200) {
     	return response()
             ->json(['code' => $status,'message' => $message, 'content' => $content], $code)
             ->header('Content-Type', 'application/json');
