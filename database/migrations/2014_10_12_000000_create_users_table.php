6672d17fe9f544a578839c02738e6aec48131cae
harkce <habibgarut@gmail.com>
2017-11-16 02:52:40 +0700

    Edit user migration

--- database/migrations/2014_10_12_000000_create_users_table.php
+++ database/migrations/2014_10_12_000000_create_users_table.php
@@ -16,10 +16,13 @@ class CreateUsersTable extends Migration
         Schema::create('users', function (Blueprint $table) {
             $table->increments('id');
             $table->string('name');
+            $table->string('gender');
+            $table->string('nim');
+            $table->string('phone');
+            $table->string('address');
             $table->string('email')->unique();
             $table->string('password');
-            $table->rememberToken();
-            $table->timestamps();
+            $table->boolean('is_tentor')->default(false);
         });
     }
 
